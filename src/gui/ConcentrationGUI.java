09673049a09f7729b6a61929b58cdf50a871b1fd
awb8593 <awb8593@rit.edu>
2021-04-03 21:09:00 -0400

    Setup javaFX and implemented start to create the GUI

--- src/gui/ConcentrationGUI.java
+++ src/gui/ConcentrationGUI.java
@@ -11,6 +11,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.Card;
 import model.ConcentrationModel;
@@ -21,11 +22,20 @@ import java.util.*;
 /**
  * The ConcentrationGUI application is the UI for Concentration.
  *
- * @author YOUR NAME HERE
+ * @author Adrian Burgos awb8593
  */
 public class ConcentrationGUI extends Application
         implements Observer< ConcentrationModel, Object > {
 
+    /**
+     * DOCUMENTATION HERE
+     *
+     * @throws Exception
+     */
+    @Override
+    public void init() throws Exception {
+        System.out.println("init: Initialize and connect to model!");
+    }
 
     /**
      * YOUR DOCUMENTATION HERE
@@ -35,6 +45,26 @@ public class ConcentrationGUI extends Application
      */
     @Override
     public void start( Stage stage ) throws Exception {
+        BorderPane layout = new BorderPane();
+        Text title = new Text("Concentration");
+        GridPane cards = new GridPane();
+        Button reset = new Button("Reset");
+        Button undo = new Button("Undo");
+        Button cheat = new Button("Cheat");
+
+        layout.getChildren().addAll(title, cards, reset, undo, cheat);
+        stage.setScene(new Scene(layout));
+        stage.setTitle("Concentration");
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 4; col++) {
+                ImageView card = new ImageView(new Image(getClass().getResourceAsStream("resources/pokeball.png")));
+                Button cardButton = new Button();
+                cardButton.setGraphic(card);
+                cards.add(cardButton, row, col);
+            }
+        }
+
+        stage.show();
 
     }
 
